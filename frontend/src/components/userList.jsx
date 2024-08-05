a3bcb9a41ae9b641d31019405658413520c81866
Akaza22 <rmrbintang22@gmail.com>
2024-08-05 13:41:02 +0700

    a

--- frontend/src/components/userList.jsx
+++ frontend/src/components/userList.jsx
@@ -1,66 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const UserList = () => {
-    const [users, setUsers] = useState([]);
-    const apiUrl = process.env.REACT_APP_API_URL;
+  const [users, setUser] = useState([]);
 
-    const getUsers = async () => {
-        try {
-            const response = await axios.get(`${apiUrl}/users`);
-            setUsers(response.data);
-        } catch (error) {
-            console.error("There was an error fetching the users!", error);
-        }
-    };
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    
-    useEffect(() => {
-        getUsers();
-    }, []);
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:5000/api/users");
+    setUser(response.data);
+  };
 
-    const deleteUser = async (id) => {
-        try {
-            await axios.delete(`${apiUrl}/users/${id}`);
-            getUsers();
-        } catch (error) {
-            console.error("There was an error deleting the user!", error);
-        }
-    };
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/users/${id}`);
+      getUsers();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="columns">
-            <div className="column is-half">
-                <Link to="add" className="button is-info">Add New</Link>
-                <table className="table is-striped is-fullwidth mt-5">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Gender</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user, index) => (
-                            <tr key={user._id}>
-                                <td>{index + 1}</td>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.gender}</td>
-                                <td>
-                                    <Link to={`edit/${user._id}`} className="button is-primary is-small">Edit</Link>
-                                    <button onClick={() => deleteUser(user._id)} className="button is-danger is-small">Delete</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div className="columns mt-5">
+      <div className="column is-half">
+        <Link to="add" className="button is-success">
+          Add New
+        </Link>
+        <table className="table is-striped is-fullwidth mt-2">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <Link
+                    to={`edit/${user._id}`}
+                    className="button is-info is-small mr-1"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => deleteUser(user._id)}
+                    className="button is-danger is-small"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
